3292e2257fc6db28115a3f55d3b80c536ee37f0c
catfish03 <catfish03@tutanota.com>
2025-08-28 22:14:51 +0300

    board: mediatek: add mt6572 board

---
+++ board/mediatek/mt6572/board.c
@@ -0,0 +1,9 @@
+/* 
+ * All of the important code happens in 
+ * arch/arm/mach-mediatek/mt6572/...; this is just a placeholder
+ * so that U-Boot doesn't complain.
+ */
+int board_init(void)
+{
+	return 0;
+}
---
+++ include/configs/mt6572.h
@@ -0,0 +1,6 @@
+#ifndef __MT6572_H
+#define __MT6572_H
+
+#define CFG_SYS_SDRAM_BASE		0x80000000
+
+#endif /* ifdef __MT6572_H */
