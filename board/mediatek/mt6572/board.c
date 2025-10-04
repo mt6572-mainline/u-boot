829c07a668c3d7c3b4b839c9748571a318bbfec0
catfish03 <catfish03@tutanota.com>
2025-10-04 15:41:10 +0300

    board: mediatek: drop mt6572 board

--- board/mediatek/mt6572/board.c
+++
@@ -1,9 +0,0 @@
-/* 
- * All of the important stuff happens in 
- * arch/arm/mach-mediatek/mt6572/init.c; this is here
- * just so that U-Boot doesn't complain about an undefined reference.
- */
-int board_init(void)
-{
-	return 0;
-}
